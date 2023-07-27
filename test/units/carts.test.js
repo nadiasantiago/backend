77d425acaad8325bae7f0d0d0e3fd1269ee51c6a
Nadia Santiago <nadusantiago@gmail.com>
2023-07-26 22:18:11 -0300

    cart test

---
+++ test/units/carts.test.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import config from "../../src/config/config.js";
+import chai from "chai";
+
+import Carts from "../../src/dao/dbManagers/CartManager.js";
+import Products from "../../src/dao/dbManagers/ProductManager.js";
+
+const { dbUrlTest } = config;
+
+const expect = chai.expect;
+
+describe("set de pruebas cartDao", function () {
+  this.timeout(10000);
+  this.cartId = "";
+  this.prodId  = '';
+
+  before(function () {
+    mongoose.connect(dbUrlTest);
+    this.cartDao = new Carts();
+    this.productDao = new Products();
+  });
+
+  after(function () {
+    mongoose.connection.collections.carts.drop();
+    mongoose.connection.collections.products.drop();
+  });
+
+  it('Crea un carrito con exito con ID', async function(){
+    const result = await this.cartDao.addCart()
+    this.cartId = result._id.toString()
+
+    expect(result).to.have.property('_id')
+  });
+
+  it('Agrega un producto al carrito con exito', async function(){
+    const generarCodigoAleatorio = (longitud) => {
+      const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let codigo = '';
+      for (let i = 0; i < longitud; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        codigo += caracteres.charAt(indiceAleatorio);
+      }
+      return codigo;
+    }
+
+    const productMock = {
+      title: "producto de prueba",
+      description: "este es un producto de prueba",
+      code: generarCodigoAleatorio(8),
+      price: 50,
+      stock: 10,
+      category: "comestibles",
+    };
+
+    const {_id: prodId} = await this.productDao.create(productMock);
+    this.prodId = prodId
+
+    const result = await this.cartDao.addToCart(this.cartId, this.prodId)
+    const cart = await this.cartDao.getCartById(this.cartId)
+
+    expect(result).to.have.property('modifiedCount').eq(1)
+    expect(cart.products[0]).to.have.property('pid')
+    expect(cart.products[0].pid._id).to.deep.eq(prodId)
+
+  })
+
+  it('Se vacia el carrito con exito', async function(){
+    const result = await this.cartDao.deleteAllFromCart(this.cartId)
+    const cart = await this.cartDao.getCartById(this.cartId)    
+    expect(result).to.have.property('modifiedCount').eq(1)
+    expect(cart.products).to.have.lengthOf(0)
+  })
+});
--- test/units/products.test.js
+++ test/units/products.test.js
@@ -16,14 +16,22 @@ describe("set de pruebas productDao", function () {
     this.productDao = new Products();
   });
 
-  after(function () {
-    mongoose.connection.collections.products.drop();
-  });
   it("Crea un producto con exito", async function () {
+
+    const generarCodigoAleatorio = (longitud) => {
+      const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let codigo = '';
+      for (let i = 0; i < longitud; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        codigo += caracteres.charAt(indiceAleatorio);
+      }
+      return codigo;
+    }
+
     const productMock = {
       title: "producto de prueba",
       description: "este es un producto de prueba",
-      code: "prueba123",
+      code: generarCodigoAleatorio(8),
       price: 50,
       stock: 10,
       category: "comestibles",
@@ -38,4 +46,12 @@ describe("set de pruebas productDao", function () {
     const {docs} = await this.productDao.getProducts()
     expect(docs).to.be.a('array')
   })
+  it('se actualiza el producto con exito', async function(){
+    const result = await this.productDao.update({_id: this.prodId},{price: 10})
+    const product = await this.productDao.getProductById(this.prodId)
+
+    expect(product.price).to.be.eq(10)
+    expect(result).to.have.property('modifiedCount').eq(1)
+  })
+
 });
