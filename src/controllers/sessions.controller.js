b3d68f58230a5153d019f722a39f535ebed437cf
Nadia Santiago <nadusantiago@gmail.com>
2023-08-02 16:34:52 -0300

    cambio de rol con chequeo de documentacion

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -7,7 +7,7 @@ import { mailingService } from "../services/mailing.service.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await sessionService.getUser({email});
+  const user = await sessionService.getUser({ email });
   if (!user)
     return res
       .status(401)
@@ -21,7 +21,10 @@ export const login = async (req, res) => {
   const jwtUser = JSON.parse(JSON.stringify(userDto));
   const token = jwt.sign(jwtUser, config.jwtSecret, { expiresIn: "24h" });
 
-  await sessionService.updateUser({email:jwtUser.email}, {last_connection: new Date()})
+  await sessionService.updateUser(
+    { email: jwtUser.email },
+    { last_connection: new Date() }
+  );
 
   return res.cookie("jwtCookie", token, { httpOnly: true }).send({
     status: "success",
@@ -41,7 +44,9 @@ export const githubcallback = (req, res) => {
 };
 
 export const register = async (req, res) => {
-  return res.status(201).send({ status: "success", message: "Usuario registrado" });
+  return res
+    .status(201)
+    .send({ status: "success", message: "Usuario registrado" });
 };
 
 export const failRegister = (req, res) => {
@@ -56,7 +61,7 @@ export const current = (req, res) => {
 export const logout = async (req, res) => {
   const token = req.cookies.jwtCookie;
   const { email } = jwt.verify(token, config.jwtSecret);
-  await sessionService.updateUser({email}, {last_connection: new Date()})
+  await sessionService.updateUser({ email }, { last_connection: new Date() });
 
   return res
     .clearCookie("jwtCookie")
@@ -66,8 +71,8 @@ export const logout = async (req, res) => {
 export const emailToRestorePassword = async (req, res) => {
   try {
     const { email } = req.body;
-    console.log(email)
-    const user = await sessionService.getUser({email});
+    console.log(email);
+    const user = await sessionService.getUser({ email });
     if (!user)
       return res
         .status(401)
@@ -77,62 +82,88 @@ export const emailToRestorePassword = async (req, res) => {
     const jwtUser = JSON.parse(JSON.stringify(userDto));
     const token = jwt.sign(jwtUser, config.jwtSecret, { expiresIn: "1h" });
 
-    await mailingService.sendEmail(token, userDto)
+    await mailingService.sendEmail(token, userDto);
 
     return res.status(200).send({
       status: "success",
       message: "Password reset email sent",
     });
-
-
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
-export const resetPassword = async (req, res)=>{
+export const resetPassword = async (req, res) => {
   try {
-    const {newPassword, token} = req.body
-    const tokenPayload = jwt.verify(token, config.jwtSecret, {ignoreExpiration: true})
-    if (Date.now()/1000 > tokenPayload.exp){
-      return res.redirect('/restorePassword')
+    const { newPassword, token } = req.body;
+    const tokenPayload = jwt.verify(token, config.jwtSecret, {
+      ignoreExpiration: true,
+    });
+    if (Date.now() / 1000 > tokenPayload.exp) {
+      return res.redirect("/restorePassword");
     }
-    const {email} = tokenPayload
-    const user = await sessionService.getUser({email})
+    const { email } = tokenPayload;
+    const user = await sessionService.getUser({ email });
 
     if (isValidPassword(user, newPassword))
-    return res
-      .status(401)
-      .send({ status: "error", error: "La contraseña debe ser distinta a la anterior" });
+      return res
+        .status(401)
+        .send({
+          status: "error",
+          error: "La contraseña debe ser distinta a la anterior",
+        });
 
     const hashedPassword = creatHash(newPassword);
-    const updatedPassword = await sessionService.updateUser({email}, {password:hashedPassword})
-    if(!updatedPassword)
-      return res.status(500).send({status:'error', error:'Error al actualizar la contraseña'})
+    const updatedPassword = await sessionService.updateUser(
+      { email },
+      { password: hashedPassword }
+    );
+    if (!updatedPassword)
+      return res
+        .status(500)
+        .send({ status: "error", error: "Error al actualizar la contraseña" });
 
-    res.status(200).send({status:'success', message:'contraseña cambiada con exito'})
+    res
+      .status(200)
+      .send({ status: "success", message: "contraseña cambiada con exito" });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-export const changeRole = async (req, res)=>{
+export const changeRole = async (req, res) => {
   try {
-      const {uid} = req.params;
-      if (!uid) return res.status(400).send({status:'error', error:'campo incompleto'})
-      const user = await sessionService.getUser({_id:uid})
-      console.log(user)
-      if(user.rol == 'user'){
-        user.rol = 'premium'
-      }else{
-        user.rol = 'user'
+    const { uid } = req.params;
+    const requiredStatus = ["identificacion", "direccion", "resumen"];
+    if (!uid)
+      return res
+        .status(400)
+        .send({ status: "error", error: "campo incompleto" });
+    const user = await sessionService.getUser({ _id: uid });
+    const userStatus = user.status;
+    const missingStatus = requiredStatus.filter((e) => !userStatus.includes(e));
+    if (missingStatus.length == 0) {
+      if (user.rol == "user") {
+        user.rol = "premium";
+      } else {
+        user.rol = "user";
       }
       const changeRole = await sessionService.changeRole(uid, user.rol);
-      if(!changeRole)
-        return res.status(500).send({status:'error', error:'Error al actualizar el rol'})
-      res.status(200).send({status:'success', message:'Rol cambiado con exito'})
-    
+      if (!changeRole)
+        return res
+          .status(500)
+          .send({ status: "error", error: "Error al actualizar el rol" });
+      return res
+        .status(200)
+        .send({ status: "success", message: "Rol cambiado con exito" });
+    }
+    return res
+      .status(500)
+      .send({
+        status: "error",
+        error: `Falta documentacion, por favor complete ${missingStatus.join(", ")}`,
+      });
   } catch (error) {
-    console.log(error)
-  }  
-}
+    console.log(error);
+  }
+};
