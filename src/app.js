dc6545b633e2466f398bc4bba5f7dd195666d2be
Nadia Santiago <nadusantiago@gmail.com>
2023-09-02 02:42:56 -0300

    views

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ app.engine(
     defaultLayout: "login",
   })
 );
-app.set("views", __dirname + '/views');
+app.set("views", __dirname + 'views');
 app.set("view engine", "handlebars");
 app.use("/", viewsRouter);
 app.use("/loggerTest", loggerTestRouter);
