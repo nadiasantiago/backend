74c61d83657db7b409780dde80854825d6b5ec05
Nadia Santiago <nadusantiago@gmail.com>
2023-09-02 03:00:33 -0300

    views correcciones

--- src/app.js
+++ src/app.js
@@ -68,10 +68,10 @@ app.engine(
         return obj[key];
       }
     },
-    defaultLayout: "login",
+    defaultLayout: "main",
   })
 );
-app.set("views", __dirname + 'views');
+app.set("views", __dirname + '/views');
 app.set("view engine", "handlebars");
 app.use("/", viewsRouter);
 app.use("/loggerTest", loggerTestRouter);
